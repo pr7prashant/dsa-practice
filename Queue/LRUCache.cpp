777d918ca8542a1cced958f019ae8873baa1602d
Prashant <pr7prashant@gmail.com>
2021-06-05 18:34:00 +0530

    LRU Cache

---
+++ Queue/LRUCache.cpp
@@ -0,0 +1,95 @@
+/*
+
+**************** Problem Description ****************
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+
+  Example 1:
+
+  Input
+  ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+  [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+
+  Output
+  [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+  Explanation
+  LRUCache lRUCache = new LRUCache(2);
+  lRUCache.put(1, 1); // cache is {1=1}
+  lRUCache.put(2, 2); // cache is {1=1, 2=2}
+  lRUCache.get(1);    // return 1
+  lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+  lRUCache.get(2);    // returns -1 (not found)
+  lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+  lRUCache.get(1);    // return -1 (not found)
+  lRUCache.get(3);    // return 3
+  lRUCache.get(4);    // return 4
+
+*/
+
+class LRUCache {
+private:
+    int cap;
+    list<pair<int, int>> ls;
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+      auto it = find_if(
+        ls.begin(),
+        ls.end(),
+        [key](pair<int, int> p) { return p.first == key; }
+      );
+      
+      if (it == ls.end()) return -1;
+      
+      pair<int, int> p = { key, it->second };
+      ls.push_back(p);
+      ls.erase(it);
+      
+      return p.second;
+    }
+    
+    void put(int key, int value) {     
+        auto it = find_if(
+          ls.begin(),
+          ls.end(),
+          [key](pair<int, int> p) { return p.first == key; }
+        );
+      
+        pair<int, int> p = { key, value };
+      
+        if (it == ls.end()) {
+          pair<int, int> p = { key, value };
+          
+          if (ls.size() == cap) ls.pop_front();
+          
+          ls.push_back(p); 
+        } else {
+          ls.push_back(p);
+          ls.erase(it);
+        }
+    }
+};
+
+/*
+
+**************** Logic ****************
+Use list as queue for LRU.
+
+*/
