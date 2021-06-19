ab423cf998e5ec02311d007e5b1160234ceef828
Prashant <pr7prashant@gmail.com>
2021-06-19 17:48:56 +0530

    remove extra line breaks

--- LinkedList/QuickSort.cpp
+++ LinkedList/QuickSort.cpp
@@ -4,9 +4,6 @@
 Sort the given Linked List using quicksort, which takes O(n^2) time in worst case and O(nLogn) in average and best cases,
 otherwise you may get TLE.
 
-
-
-
   Example 1 : 
 
   Input:
