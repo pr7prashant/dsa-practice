5e58e44994095cd41a2154d2357af1e34c0dad53
Prashant Rawat <pr7prashant@gmail.com>
2021-09-06 22:09:49 +0530

    Update HuffmanEncoding.cpp

--- Heap/HuffmanEncoding.cpp
+++ Heap/HuffmanEncoding.cpp
@@ -52,44 +52,43 @@ struct comp {
 class Solution
 {
 	public:
-	    void preorder(Node *root, vector<string> &ans, string path) {
-	        if (!root) return;
-	        
-	        if (!root->left && !root->right) ans.push_back(path);
-	        
-	        preorder(root->left, ans, path + '0');
-	        preorder(root->right, ans, path + '1');
-	    }
+	void preorder(Node *root, vector<string> &ans, string path) {
+		if (!root) return;
+
+		if (!root->left && !root->right) ans.push_back(path);
+
+		preorder(root->left, ans, path + '0');
+		preorder(root->right, ans, path + '1');
+	}
 	    
-		vector<string> huffmanCodes(string S,vector<int> f,int N)
-		{
-		   // build min heap
-		   priority_queue<Node*, vector<Node*>, comp> pq;
-		   for (int i = 0; i < N; i++) {
-		       pq.push(new Node(S[i], f[i]));
-		   }
-		   
-		   // create huffman tree from min heap
-		   Node *root;
-		   while (pq.size() > 1) {
-		       Node *left = pq.top();
-		       pq.pop();
-		       Node *right = pq.top();
-		       pq.pop();
-		       
-		       root = new Node('$', left->freq + right->freq);
-		       root->left = left;
-		       root->right = right;
-		       
-		       pq.push(root);
-		   }
-		   
-		   // traverse tree to generate codes
-		   vector<string> ans;
-		   preorder(root, ans, "");
-		   
-		   return ans;
-		}
+	vector<string> huffmanCodes(string S,vector<int> f,int N) {
+		 // build min heap
+		 priority_queue<Node*, vector<Node*>, comp> pq;
+		 for (int i = 0; i < N; i++) {
+		 	pq.push(new Node(S[i], f[i]));
+		 }
+
+		 // create huffman tree from min heap
+		 Node *root;
+		 while (pq.size() > 1) {
+			 Node *left = pq.top();
+			 pq.pop();
+			 Node *right = pq.top();
+			 pq.pop();
+
+			 root = new Node('$', left->freq + right->freq);
+			 root->left = left;
+			 root->right = right;
+
+			 pq.push(root);
+		 }
+
+		 // traverse tree to generate codes
+		 vector<string> ans;
+		 preorder(root, ans, "");
+
+		 return ans;
+	}
 };
 
 int main(){
