dbe48697b44b708e591d92fd88519f3a48e614c6
Prashant Rawat <pr7prashant@gmail.com>
2021-09-12 22:07:28 +0530

    Update MeetingRoom.cpp

--- Greedy/MeetingRoom.cpp
+++ Greedy/MeetingRoom.cpp
@@ -2,7 +2,10 @@
 
 **************** Problem Description ****************
 
-Given an integer array nums and an integer k, return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.
+There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
+What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
+
+Note: Start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
 
 
   Example 1 : 
