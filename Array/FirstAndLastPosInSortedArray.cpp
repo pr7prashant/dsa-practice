27835396a24ff780ad5863a833ac61cf055493a9
Prashant Rawat <pr7prashant@gmail.com>
2021-12-01 11:22:15 +0530

    First and Last Position of Element in Sorted Array

--- Array/FirstAndLastPosInSortedArray.cpp
+++ Array/FirstAndLastPosInSortedArray.cpp
@@ -1,24 +1,28 @@
 /*
 
 **************** Problem Description ****************
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
-and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
-  Example : 
+If target is not found in the array, return [-1, -1].
 
-  Input:
-  intervals = [[1,3],[2,6],[8,10],[15,18]]
+You must write an algorithm with O(log n) runtime complexity.
 
-  Output: [[1,6],[8,10],[15,18]]
 
-  Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+    Example1 : 
 
-  Input:
-  intervals = [[1,4],[4,5]]
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
 
-  Output: [[1,5]]
+    Example2 : 
 
-  Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+    Input: nums = [5,7,7,8,8,10], target = 6
+    Output: [-1,-1]
+
+
+    Example3 : 
+
+    Input: nums = [], target = 0
+    Output: [-1,-1]
 
 */
 
@@ -70,7 +74,6 @@ public:
 /*
 
 **************** Logic ****************
-Sort the intervals vector. Add the first interval in result vector.
-Iterate over intervals vector and compare it with the latest interval in result vector.
+User binary search twice. One to find first position and one for last position. 
 
 */
