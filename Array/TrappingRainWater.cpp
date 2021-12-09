ded6349b3cfeef36052fb4e1f039206d55388e65
Prashant Rawat <pr7prashant@gmail.com>
2021-12-09 09:08:50 +0530

    update question - trapping rain water

--- Array/TrappingRainWater.cpp
+++ Array/TrappingRainWater.cpp
@@ -1,77 +1,50 @@
 /*
 
 **************** Problem Description ****************
-Given an array arr[] of N non-negative integers representing the height of blocks.
-If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
-  Example : 
 
-  Input:
-  N = 6
-  arr[] = {3,0,0,2,0,4}
+    Example 1 : 
 
-  Output: 10
+    Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+    Output: 6
+
+
+    Example 2 : 
+
+    Input: height = [4,2,0,3,2,5]
+
+    Output: 9
 
 */
 
-#include<bits/stdc++.h>
-
-using namespace std;
-
-class Solution{
-
-    // Function to find the trapped water between the blocks.
-    public:
-    int trappingWater(int arr[], int n){
-        vector<int> maxL;
-        vector<int> maxR;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int len = height.size();
         
-        maxL.push_back(arr[0]);
-        for (int i=1; i<n; i++) {
-            maxL.push_back(max(maxL[i-1], arr[i]));
+        vector<int> maxL(len);
+        maxL[0] = 0;
+        for (int i = 1; i < len; i++) {
+            maxL[i] = max(height[i-1], maxL[i-1]);
         }
         
-        reverse(arr, arr + n);
-        maxR.push_back(arr[0]);
-        for (int i=1; i<n; i++) {
-            maxR.push_back(max(maxR[i-1], arr[i]));
+        vector<int> maxR(len);
+        maxR[len - 1] = 0;
+        for (int i = len - 2; i >= 0; i--) {
+            maxR[i] = max(height[i+1], maxR[i+1]);
         }
         
-        int total = 0;
-        for (int i=0; i<n; i++) {
-            total += min(maxL[i], maxR[n-i-1]) - arr[i];
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            int top = min(maxL[i], maxR[i]);
+            ans += max(top - height[i], 0);
         }
         
-        return total;
+        return ans;
     }
 };
-
-int main(){
-    
-    int t;
-    //testcases
-    cin >> t;
-    
-    while(t--){
-        int n;
-        
-        //size of array
-        cin >> n;
-        
-        int a[n];
-        
-        //adding elements to the array
-        for(int i =0;i<n;i++){
-            cin >> a[i];            
-        }
-        Solution obj;
-        //calling trappingWater() function
-        cout << obj.trappingWater(a, n) << endl;
-        
-    }
-    
-    return 0;
-}
 
 /*
 
