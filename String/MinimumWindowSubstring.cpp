6f995b71b39240768dab914e3f67ebfe09c9df6b
Prashant Rawat <pr7prashant@gmail.com>
2021-12-18 12:28:44 +0530

    update solution - Minimum window substring

--- String/MinimumWindowSubstring.cpp
+++ String/MinimumWindowSubstring.cpp
@@ -1,6 +1,7 @@
 /*
 
 **************** Problem Description ****************
+
 Given two strings s and t of lengths m and n respectively,
 return the minimum window in s which will contain all the characters in t.
 If there is no such window in s that covers all characters in t, return the empty string "".
@@ -24,40 +25,28 @@ Note that If there is such a window, it is guaranteed that there will always be 
 class Solution {
 public:
     string minWindow(string s, string t) {
-      map<char, int> mp, mp2;
+      vector<int> mp(128, 0);
       int i=0, j=0;
-      int slen = s.length();
-      int tlen = t.length();
       string ans = "";
 
-      if (tlen > slen) return "";
+      if (t.length() > s.length()) return "";
         
-      for (int i=0; i<tlen; i++) mp[t[i]] += 1;
+      for (int i = 0; i < t.length(); i++) mp[t[i]]++;
       
-      int count = mp.size();
+      int count = t.length();
 
-      while(j < slen) {
-        if (count > 0) {
-          if (mp.find(s[j]) != mp.end()) {
-            mp[s[j]] -= 1;
-            if (mp[s[j]] == 0) count--;
-          }
-        }
+      while(j < s.length()) {
+        if (mp[s[j]]-- > 0) count--;
         
-        if (count == 0) {          
-          while (i <= j && count == 0) {
+        while (i <= j && count == 0) {
             if (ans == "" || j - i + 1 < ans.length()) {
               ans = s.substr(i, j - i + 1);
             }
-            
-            if (mp.find(s[i]) != mp.end()) {
-              mp[s[i]] += 1;
-              if (mp[s[i]] == 1) count++;
-            }
-            
+
+            if (mp[s[i]]++ == 0) count++;
+
             i++;
-          }
-        }      
+        }   
   
         j++;
       }
