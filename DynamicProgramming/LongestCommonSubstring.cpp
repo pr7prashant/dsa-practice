6d18d73015f8d24ddff87974049bce7f41ce998a
Prashant Rawat <pr7prashant@gmail.com>
2022-02-12 11:40:17 +0530

    update solution : Longest Common Substring

--- DynamicProgramming/LongestCommonSubstring.cpp
+++ DynamicProgramming/LongestCommonSubstring.cpp
@@ -29,46 +29,55 @@ using namespace std;
 
 class Solution{
     public:
-    int helper(string& S1, string& S2, int n, int m, int msf, int ans) {
-        if (n == 0 || m == 0) return max(msf, ans);
+    int ans = 0;
+    
+    int helper(string& S1, string& S2, int n, int m, vector<vector<int>>& dp) {
+        if (n == 0 || m == 0) return 0;
+        
+        if (dp[n][m] != -1) return dp[n][m];
+        
+        helper(S1, S2, n-1, m, dp);
+        helper(S1, S2, n, m-1, dp);
         
         if (S1[n - 1] == S2[m - 1]) {
-            msf += 1;
-            ans = max(msf, ans);
-            return helper(S1, S2, n-1, m-1, msf, ans);
+            dp[n][m] = 1 + helper(S1, S2, n-1, m-1, dp);
+            ans = max(ans, dp[n][m]);
+            return dp[n][m];
         }
         
-        return max(helper(S1, S2, n-1, m, 0, ans), helper(S1, S2, n, m-1, 0, ans));
+        return dp[n][m] = 0;
     }
     
     int longestCommonSubstr(string S1, string S2, int n, int m) {
-        // Recursive Brute Force
-        // return helper(S1, S2, n, m, 0, 0);
+        // Memoization
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        helper(S1, S2, n, m, dp);
+        return ans;
         
         // Tabulation
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1));
         
-        for (int j = 0; j < m + 1; j++) dp[0][j] = 0;
-        for (int i = 0; i < n + 1; i++) dp[i][0] = 0;
+        // for (int j = 0; j < m + 1; j++) dp[0][j] = 0;
+        // for (int i = 0; i < n + 1; i++) dp[i][0] = 0;
         
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                if (S1[i-1] == S2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = 0;
-                }
-            }
-        }
+        // for (int i = 1; i < n + 1; i++) {
+        //     for (int j = 1; j < m + 1; j++) {
+        //         if (S1[i-1] == S2[j-1]) {
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         } else {
+        //             dp[i][j] = 0;
+        //         }
+        //     }
+        // }
         
-        int mx = 0;
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                mx = max(mx, dp[i][j]);
-            }
-        }
+        // int mx = 0;
+        // for (int i = 0; i < n + 1; i++) {
+        //     for (int j = 0; j < m + 1; j++) {
+        //         mx = max(mx, dp[i][j]);
+        //     }
+        // }
         
-        return mx;
+        // return mx;
     }
 };
 
