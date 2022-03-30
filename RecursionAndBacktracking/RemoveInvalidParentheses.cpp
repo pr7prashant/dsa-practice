117269278a381935c10d998d2e9b8cf99c2195e5
Prashant Rawat <pr7prashant@gmail.com>
2022-03-30 08:12:41 +0530

    update solution : remove invalid parenthesis

--- RecursionAndBacktracking/RemoveInvalidParentheses.cpp
+++ RecursionAndBacktracking/RemoveInvalidParentheses.cpp
@@ -29,49 +29,48 @@ Return all the possible results. You may return the answer in any order.
 
 class Solution {
 public:
-    vector<string> ans;
+    vector<string> res;
     unordered_map<string, bool> mp;
     
-    int countInvalid(string s) {
+    int countInvalid(string& s) {
         stack<char> stk;
-        
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s[i] != '(' && s[i] != ')') continue;
-            else if (stk.empty() || s[i] == '(') stk.push(s[i]);
-            else if (s[i] == ')' && stk.top() == '(') stk.pop();
+            
+            if (s[i] == ')' && !stk.empty() && stk.top() == '(') stk.pop();
             else stk.push(s[i]);
         }
         
         return stk.size();
     }
     
-    void removeHelper(string s, int invalid) {
-        if (invalid < 0) return;
+    void solve(string& s, int invalids) {
+        if (invalids < 0) return;
         
-        if (invalid == 0) {
-            int invalidNow = countInvalid(s);
-            if (invalidNow == 0) {
-                ans.push_back(s);
-                return;
-            }
+        if (invalids == 0 && countInvalid(s) == 0) {
+            res.push_back(s);
+            return;
         }
         
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != '(' && s[i] != ')') continue;
+            
             string left = s.substr(0, i);
-            string right = s.substr(i+1);
+            string right = s.substr(i+1, s.length() - i - 1);
             string temp = left + right;
+            
             if (mp.find(temp) == mp.end()) {
                 mp[temp] = true;
-                removeHelper(temp, invalid - 1);
+                solve(temp, invalids - 1);
             }
         }
     }
     
     vector<string> removeInvalidParentheses(string s) {
-        int invalid = countInvalid(s);
-        removeHelper(s, invalid);
-        return ans;
-    } 
+        int invalids = countInvalid(s);
+        solve(s, invalids);
+        return res;
+    }
 };
 
 /*
