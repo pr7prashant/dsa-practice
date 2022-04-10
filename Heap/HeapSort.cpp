9d2d567dd21c0c6a2cb2d8324e688bcb482a0eff
Prashant Rawat <pr7prashant@gmail.com>
2022-04-10 09:10:10 +0530

    update solution : heap sort

--- Heap/HeapSort.cpp
+++ Heap/HeapSort.cpp
@@ -28,78 +28,73 @@ The task is to sort the array elements by completing functions heapify() and bui
   
 */
 
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <stdio.h>
-  
-// Function to swap the the position of two elements
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-  }
-  
-// Print an array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
-      printf("%d ", arr[i]);
-    printf("\n");
-  }
-  
-// Driver code
-int main() {
-    int t;
-    scanf("%d",&t);
-    while (t--){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i = 0; i < n; i++){
-        scanf("%d",&arr[i]);
-    }
-    buildHeap(arr, n);
-    printArray(arr, n);
-    }
-  }// } Driver Code Ends
-
-
-//User function Template for C
-
-
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-    
-    if (left < n && arr[left] > arr[largest]) largest = left;
-    
-    if (right < n && arr[right] > arr[largest]) largest = right;
-    
-    if (largest != i) {
-        swap(&arr[i], &arr[largest]);
+class Solution {
+    public:
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
         
-        heapify(arr, n, largest);
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        
+        if (right < n && arr[right] > arr[largest]) largest = right;
+        
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
     }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n) { 
+        for (int i = n/2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n) {
+        buildHeap(arr, n);
+        
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr[i], arr[0]);
+            heapify(arr, i, 0);
+        }
+    }
+};
+
+/* Function to print an array */
+void printArray(int arr[], int size) {
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
 
-// Main function to do heap sort
-void buildHeap(int arr[], int n) {
-    // Build Heap
-    for (int i = n/2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
+// Driver program to test above functions
+int main() {
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
     }
-    
-    // Sort Heap
-    for (int i = n - 1; i >= 0; i--) {
-        swap(&arr[i], &arr[0]);
-        
-        heapify(arr, i, 0);
-    }
+    return 0;
 }
 
 /*
 
 **************** Logic ****************
 Build Tree from bottom to top from last non leaf node to root.
-Note: The swap function is defined on top and is not STL swap.
 
 */
