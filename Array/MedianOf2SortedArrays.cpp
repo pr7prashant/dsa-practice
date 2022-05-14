6eed17d003f4588e88ddde64b5f2a3d1fdbca3bd
Prashant Rawat <pr7prashant@gmail.com>
2022-05-14 07:27:56 +0530

    update solution : median of 2 sorted arrays

--- Array/MedianOf2SortedArrays.cpp
+++ Array/MedianOf2SortedArrays.cpp
@@ -49,37 +49,32 @@ The overall run time complexity should be O(log (m+n)).
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size(), len2 = nums2.size();
+        int s1 = nums1.size(), s2 = nums2.size();
         
-        if (len1 > len2) return findMedianSortedArrays(nums2, nums1);
+        if (s1 > s2) return findMedianSortedArrays(nums2, nums1);
         
-        int start = 0, end = len1;
+        int start = 0, end = s1;
         
         while (start <= end) {
-            // X is nums1 and Y is nums2
-            int partitionX = (start + end) / 2;
-            int partitionY = (len1 + len2 + 1) / 2 - partitionX;
+            int mid1 = (start + end) / 2;
+            int mid2 = (s1 + s2 + 1) / 2 - mid1;
             
-            int leftXMax = partitionX - 1 < 0 ? INT_MIN : nums1[partitionX - 1];
-            int rightXMin = partitionX >= len1 ? INT_MAX : nums1[partitionX];
+            int l1 = mid1 == 0 ? INT_MIN : nums1[mid1 - 1];
+            int l2 = mid2 == 0 ? INT_MIN : nums2[mid2 - 1];
+            int r1 = mid1 == s1 ? INT_MAX : nums1[mid1];
+            int r2 = mid2 == s2 ? INT_MAX : nums2[mid2];
             
-            int leftYMax = partitionY - 1 < 0 ? INT_MIN : nums2[partitionY - 1];
-            int rightYMin = partitionY >= len2 ? INT_MAX : nums2[partitionY];
-            
-            if (leftYMax <= rightXMin && leftXMax <= rightYMin) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (double)(max(leftXMax, leftYMax) + min(rightXMin, rightYMin)) / 2;
-                }
-                
-                return max(leftXMax, leftYMax);
-            } else if (leftXMax > rightYMin) {
-                end = partitionX - 1;
-            } else {
-                start = partitionX + 1;
+            if (l1 <= r2 && l2 <= r1) {
+                if ((s1 + s2) & 1) return max(l1, l2);
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            } else if (l1 > r2) {
+                end = mid1 - 1;
+            } else if (l2 > r1) {
+                start = mid1 + 1;
             }
         }
         
-        return -1;
+        return 0.0;
     }
 };
 
